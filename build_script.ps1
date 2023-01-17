a0bcc5ff09e061301c89c31b2b9d0c9665e0a051
ravinawale <96644377+ravinawale@users.noreply.github.com>
2023-01-17 17:25:36 +0530

    Update build_script.ps1

--- build_script.ps1
+++ build_script.ps1
@@ -1,4 +1,4 @@
 $zip_folder = mkdir ("build" + " " +($(get-date -f MM-dd-yyyy-HH-mm-ss)))
 Move-Item -Path "dist/app.exe" -Destination $zip_folder
 Compress-Archive -Path $zip_folder -DestinationPath $zip_folder
-jfrog rt u "build*.zip" localjfrogrepo --url=http://localhost:8082/
+jfrog rt u "build*.zip" localjfrogrepo --url=http://localhost:8082/artifactory

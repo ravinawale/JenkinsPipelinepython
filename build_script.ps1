58b3ebc3e642fb4ff0aa66d09212336c1f29ae19
ravinawale <96644377+ravinawale@users.noreply.github.com>
2023-01-17 17:54:24 +0530

    Update build_script.ps1

--- build_script.ps1
+++ build_script.ps1
@@ -3,4 +3,6 @@ Move-Item -Path "dist/app.exe" -Destination $zip_folder
 Compress-Archive -Path $zip_folder -DestinationPath $zip_folder
 jfrog config remove
 jfrog config s 
+jfrog config add artifactory-demo --url=http://localhost:8082/ --user=admin --password=ravi@Jfrog70
+jfrog config s
 jfrog rt u "build*.zip" localjfrogrepo --url=http://localhost:8082/artifactory
